35098033005335460aa592407e86ca4ca0c12041
zjr <zhaijiren0@163.com>
2020-04-22 16:15:05 +0800

    vcs再测试

--- src/main/java/com/imooc/demo/controller/Another.java
+++ src/main/java/com/imooc/demo/controller/Another.java
@@ -2,6 +2,6 @@ package com.imooc.demo.controller;
 
 public class Another {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Test111");
     }
 }
